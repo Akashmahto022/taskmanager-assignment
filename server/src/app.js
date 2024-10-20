8dd544c5eb4fac413e2253606515c020adfb1dd1
Akash Kumar <116185605+Akashmahto022@users.noreply.github.com>
2024-10-20 14:00:10 +0530

    Update app.js

--- server/src/app.js
+++ server/src/app.js
@@ -9,7 +9,7 @@ const app = express()
 
 app.use(
     cors({
-        origin: process.env.CORS_ORIGIN || 'https://taskmanager-gvc6.onrender.com',
+        origin: process.env.CORS_ORIGIN,
         credentials: true
     })
 )
